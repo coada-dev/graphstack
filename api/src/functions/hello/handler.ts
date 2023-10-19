c2cfebc311a193f354bc97a85687c737fe2461cb
listenrightmeow <mike@fatt.ist>
2023-10-19 14:29:25 -0700

    stable ls sls hot reload save

--- api/src/functions/hello/handler.ts
+++ api/src/functions/hello/handler.ts
@@ -6,7 +6,7 @@ import schema from './schema';
 
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   return formatJSONResponse({
-    message: `Hello ${event.body.name}, asdf to the exciting Serverless world!`,
+    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
     event,
   });
 };

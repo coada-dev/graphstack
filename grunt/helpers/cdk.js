a95a3d14b91d355ac83510657b15412320a852be
listenrightmeow <mike@fatt.ist>
2023-11-20 11:50:28 -0800

    grunt profile var fix

--- grunt/helpers/cdk.js
+++ grunt/helpers/cdk.js
@@ -4,7 +4,7 @@ const package = require("../../package.json");
 module.exports = () => {
   const environment = grunt.config.get("environment");
   const local = environment === "local";
-  const profile = grunt.config.get("profile");
+  const profile = grunt.option("profile");
 
   return {
     bin: local ? "cdklocal" : "cdk",
